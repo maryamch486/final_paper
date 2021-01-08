f16c110fbb174c8352124b085eac024698acfdd6
maryamch486 <maryamsadiq1616@gmail.com>
2021-01-07 22:27:13 -0800

    splashscreen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Splash screen',
       theme: ThemeData(
-        primarySwatch: Colors.pink,
+        primarySwatch: Colors.blue,
       ),
       home: Splash(),
       debugShowCheckedModeBanner: false,
