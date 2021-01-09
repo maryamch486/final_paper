50dbae85cb75247b90622daeafe1ff83471ca8aa
maryamch486 <maryamsadiq1616@gmail.com>
2021-01-09 00:43:24 -0800

    q

--- lib/attractive ui/data/theme.dart
+++ lib/attractive ui/data/theme.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-Color primaryColor = Color.fromARGB(255, 58, 149, 255);
+Color primaryColor = Color.fromARGB(255, 59, 149, 255);
 Color reallyLightGrey = Colors.grey.withAlpha(25);
 ThemeData appThemeLight =
     ThemeData.light().copyWith(primaryColor: primaryColor);
